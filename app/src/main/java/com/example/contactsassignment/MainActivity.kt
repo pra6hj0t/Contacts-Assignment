18316d9027dee0a6240c89958a6537656be2c398
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-03-07 07:48:22 +0530

    data

---
+++ app/src/main/java/com/example/contactsassignment/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.contactsassignment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+
+//
+    }
+}
